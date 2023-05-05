ccfe1104db78021bc2e8e3b1f722846e4033309f
Reginald Johnson <reggie3@protonmail.com>
2023-05-05 19:53:41 -0400

    first commit

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public float deadZone = -4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
